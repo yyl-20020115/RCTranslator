779f0e80ecab7abb7757d82389c865ad8b4dcab9
Yilin Yang <yyl_20050115@hotmail.com>
2024-06-05 04:15:19 +0800

    fx

---
+++ OllamaAPI/OllamaAPIProvider.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+
+namespace OllamaAPI;
+
+public static class OllamaAPIProvider
+{
+    public static readonly string APIUrl = "http://localhost:11434/api/chat";
+
+    public static readonly HttpClient client = new();
+
+    public static readonly JsonSerializerOptions options = new()
+    {
+        Encoder = JavaScriptEncoder.Create(new System.Text.Unicode.UnicodeRange(0, 0xffff)),
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true, // 格式化输出
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+    };
+
+    public static async Task<string> DoCallAPI(string uri, string text)
+    {
+        var content = new StringContent(text, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(uri, content);
+        return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : "";
+    }
+    public class Message
+    {
+        public string Role { get; set; } = "user";
+        public string Content { get; set; } = "";
+        public override string ToString() => this.Content;
+
+    }
+    public class Request
+    {
+        public string Model { get; set; } = "";
+        public List<Message> Messages { get; set; } = [];
+        public bool Stream { get; set; } = false;
+    }
+    public class Response
+    {
+        public string Model { get; set; } = "";
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public Message Message { get; set; } = new();
+        public string DoneReason { get; set; } = "";
+        public long TotalDuration { get; set; } = 0;
+        public long LoadDuration { get; set; } = 0;
+        public long PromptEvalCount { get; set; } = 0;
+        public long PromptEvalDuration { get; set; } = 0;
+        public long EvalCount { get; set; } = 0;
+        public long EvalDuration { get; set; } = 0;
+        public override string ToString() => this.Message?.ToString() ?? "";
+
+    }
+    public static async Task<string> CallOllama(string apiUrl, string qeustion, string model = "qwen", string role = "user")
+    {
+        var text = JsonSerializer.Serialize(new Request { Model = model, Messages = [new() { Content = qeustion, Role = role }], Stream = false }, options);
+        var response = await DoCallAPI(apiUrl, text);
+        var lines = response.Split('\n');
+        var list = new List<Response>();
+        foreach (var _line in lines)
+        {
+            if (string.IsNullOrEmpty(_line)) continue;
+            var res = JsonSerializer.Deserialize<Response>(_line, options);
+            if (res != null) list.Add(res);
+        }
+        return string.Join(string.Empty, list);
+    }
+
+
+}
--- RCTranslator/Program.cs
+++ RCTranslator/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text;
 using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using OllamaAPI;
 namespace RCTranslator;
 
 public class Program
@@ -13,66 +12,6 @@ public class Program
         Dialog = 1,
         Menu = 2,
         StringTable = 3
-    }
-    public static readonly string apiUrl = "http://localhost:11434/api/chat";
-
-    public static readonly HttpClient client = new();
-
-    public static readonly JsonSerializerOptions options = new()
-    {
-        Encoder = JavaScriptEncoder.Create(new System.Text.Unicode.UnicodeRange(0, 0xffff)),
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true, // 格式化输出
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
-    };
-
-    public static async Task<string> DoCallAPI(string uri, string text)
-    {
-        var content = new StringContent(text, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync(uri, content);
-        return response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync() : "";
-    }
-    public class Message
-    {
-        public string Role { get; set; } = "user";
-        public string Content { get; set; } = "";
-        public override string ToString() => this.Content;
-
-    }
-    public class Request
-    {
-        public string Model { get; set; } = "";
-        public List<Message> Messages { get; set; } = [];
-        public bool Stream { get; set; } = false;
-    }
-    public class Response
-    {
-        public string Model { get; set; } = "";
-        public DateTime CreateAt { get; set; } = DateTime.Now;
-        public Message Message { get; set; } = new();
-        public string DoneReason { get; set; } = "";
-        public long TotalDuration { get; set; } = 0;
-        public long LoadDuration { get; set; } = 0;
-        public long PromptEvalCount { get; set; } = 0;
-        public long PromptEvalDuration { get; set; } = 0;
-        public long EvalCount { get; set; } = 0;
-        public long EvalDuration { get; set; } = 0;
-        public override string ToString() => this.Message?.ToString() ?? "";
-
-    }
-    public static async Task<string> CallOllama(string apiUrl, string qeustion, string model = "qwen", string role = "user")
-    {
-        var text = JsonSerializer.Serialize(new Request { Model = model, Messages = [new() { Content = qeustion, Role = role }], Stream = false }, options);
-        var response = await DoCallAPI(apiUrl, text);
-        var lines = response.Split('\n');
-        var list = new List<Response>();
-        foreach (var _line in lines)
-        {
-            if (string.IsNullOrEmpty(_line)) continue;
-            var res = JsonSerializer.Deserialize<Response>(_line, options);
-            if (res != null) list.Add(res);
-        }
-        return string.Join(string.Empty, list);
     }
     public static bool AskHuman = true;
 
@@ -262,7 +201,7 @@ public class Program
                     if (_line.StartsWith("CAPTION "))
                     {
                         var part = _line[8..].Trim('"');
-                        var result = part.Length > 0 ? await CallOllama(apiUrl, $"请翻译:{part}") : string.Empty;
+                        var result = part.Length > 0 ? await OllamaAPIProvider.CallOllama(OllamaAPIProvider.APIUrl, $"请翻译:{part}") : string.Empty;
                         result = AskHuman ? DoAsk(result, ref part) : result;
                         writer.WriteLine($"CAPTION \"{result}\"");
                     }
@@ -272,7 +211,7 @@ public class Program
                         if (parts.Length >= 1 && parts[0].StartsWith('"') && parts[0].EndsWith('"'))
                         {
                             parts[0] = parts[0].Trim('"');
-                            var result = parts[0].Length > 0 ? await CallOllama(apiUrl, $"请翻译:{parts[0]}") : string.Empty;
+                            var result = parts[0].Length > 0 ? await OllamaAPIProvider.CallOllama(OllamaAPIProvider.APIUrl, $"请翻译:{parts[0]}") : string.Empty;
                             result = AskHuman ? DoAsk(result, ref parts[0]) : result;
                             parts[0] = $"\"{result}\"";
                             writer.WriteLine(line[..20] + string.Join(',', parts));
@@ -297,7 +236,7 @@ public class Program
                     {
                         parts[1] = parts[1].Trim('"');
                         parts[1] = TrimTail(parts[1], out var tail);
-                        var result = parts[1].Length > 0 ? await CallOllama(apiUrl, $"请翻译:{parts[1]}") : string.Empty;
+                        var result = parts[1].Length > 0 ? await OllamaAPIProvider.CallOllama(OllamaAPIProvider.APIUrl, $"请翻译:{parts[1]}") : string.Empty;
                         result = AskHuman ? DoAsk(result, ref parts[1]) : result;
                         parts[1] = $"\"{result + tail}\"";
                         writer.WriteLine(_head + string.Join(' ', parts));
@@ -316,7 +255,7 @@ public class Program
                     {
                         parts[0] = parts[0].Trim('"');
                         parts[0] = TrimTail(parts[0], out var tail);
-                        var result = parts[0].Length > 0 ? await CallOllama(apiUrl, $"请翻译:{parts[0]}") : string.Empty;
+                        var result = parts[0].Length > 0 ? await OllamaAPIProvider.CallOllama(OllamaAPIProvider.APIUrl, $"请翻译:{parts[0]}") : string.Empty;
                         result = AskHuman ? DoAsk(result, ref parts[0]) : result;
                         parts[0] = $"\"{result + tail}\"";
                         writer.WriteLine(_head + "MENUITEM " + string.Join(',', parts));
@@ -352,7 +291,7 @@ public class Program
                 if (parts.Length == 2 && parts[1].StartsWith('"') && parts[1].EndsWith('"'))
                 {
                     parts[1] = parts[1].Trim('"');
-                    var result = await CallOllama(apiUrl, $"请翻译:{parts[1]}");
+                    var result = await OllamaAPIProvider.CallOllama(OllamaAPIProvider.APIUrl, $"请翻译:{parts[1]}");
                     result = AskHuman ? DoAsk(result, ref parts[1]) : result;
                     parts[1] = $"\"{result}\"";
                     writer.WriteLine(_head + string.Join(' ', parts));
